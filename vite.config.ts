14cb2e4ddde96943ffd3be0dc9afc6ec6ff6b3bf
Jaehyun Park <jp.inseoul@gmail.com>
2022-10-04 01:59:04 -0600

    updating path configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   /* vitest configuration */
   test: {
     globals: true,
